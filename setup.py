6d744a186f33932284d2c7b09b0239f18f075efb
adamcseresznye <csenye.chem@gmail.com>
2023-09-18 18:26:57 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This projects predicts the house prices in Belgium.',
+    author='Adam Cseresznye',
+    license='BSD-3',
+)
