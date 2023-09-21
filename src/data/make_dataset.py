e95b7b2960d17de450206e3aedcb81f983a2233e
adamcseresznye <csenye.chem@gmail.com>
2023-09-21 21:47:16 +0200

    notebooks updated

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -275,10 +275,11 @@ def immoweb_scraping_pipeline(
         )
 
         # Save the joined table to a CSV file
-        reformatted_dataframe.to_csv(
+        reformatted_dataframe.to_parquet(
             filepath.joinpath(
-                f"listings_on_page_{page}_{str(pd.Timestamp.now())[:10]}.csv"
+                f"listings_on_page_{page}_{str(pd.Timestamp.now())[:10]}.parquet.gzip"
             ),
+            compression="gzip",
             index=False,
         )
 
@@ -289,8 +290,11 @@ def immoweb_scraping_pipeline(
 
     complete_dataset = pd.concat(all_tables, axis=0)
 
-    complete_dataset.to_csv(
-        filepath.joinpath(f"complete_dataset_{str(pd.Timestamp.now())[:10]}.csv"),
+    complete_dataset.to_parquet(
+        filepath.joinpath(
+            f"complete_dataset_{str(pd.Timestamp.now())[:10]}.parquet.gzip"
+        ),
+        compression="gzip",
         index=False,
     )
 
@@ -303,7 +307,6 @@ def immoweb_scraping_pipeline(
 last_page = 4
 complete_dataset = immoweb_scraping_pipeline(
     path=utils.Configuration.RAW_DATA_PATH,
-    # path=Path("C://Users//s0212777//OneDrive - Universiteit Antwerpen//Jupyter_projects//Articles//house_price_prediction//data//raw"),
     kind_of_apartment="for_sale",
     session=session,
     last_page=last_page,
