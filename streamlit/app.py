f1a078ebec7c725b5d3e0e89f5751fb1325f550a
adamcseresznye <csenye.chem@gmail.com>
2023-10-28 19:51:14 +0200

    streamlit added

---
+++ streamlit/app.py
@@ -0,0 +1,14 @@
+import datetime
+
+import numpy as np
+import pandas as pd
+
+import streamlit as st
+
+
+def main():
+    st.title("This is my test page")
+
+
+if __name__ == "__main__":
+    main()
